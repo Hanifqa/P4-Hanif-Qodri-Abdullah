f75677d3fda70ff683a1cf6a06996b791774b71c
Hanifqa <64571819+Hanifqa@users.noreply.github.com>
2020-05-11 22:17:55 +0800

    Create Jobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+char* nim_array[10]={"f1b018060","f1b018053","f1b018077","f1b018044","f1b018040","f1b018075","f1b018019","f1b018060","f1b018056",”f1b115013”};
+	printf("nim mahasis wake- 1 = %s\n",nim_array[0]);
+	printf("nim mahasiswa ke- 2 = %s\n",nim_array[1]);
+	printf("nim mahasiswa ke- 3 = %s\n",nim_array[2]);
+	printf("nim mahasiswa ke- 4 = %s\n",nim_array[3]);
+	printf("nim mahasiswa ke- 5 = %s\n",nim_array[4]);
+	printf("nim mahasiswa ke- 6 = %s\n",nim_array[5]);
+	printf("nim mahasiswa ke- 7 = %s\n",nim_array[6]);
+	printf("nim mahasiswa ke- 8 = %s\n",nim_array[7]);
+	printf("nim mahasiswa ke- 9 = %s\n",nim_array[8]);
+}
