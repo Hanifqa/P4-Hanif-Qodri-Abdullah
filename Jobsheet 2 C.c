29f3823cd4fa5a197740f254b96f87dff37f6a2e
Hanifqa <64571819+Hanifqa@users.noreply.github.com>
2020-05-11 22:21:06 +0800

    Create Jobsheet 2 C.c
    Mengisi dan menampilkan array dinamis

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+main() {
+	string x [10];
+	printf("Masukkan NIM 1 : ") ;
+	scanf("%i",&x[0]) ;
+	printf("Masukkan NIM 2 : ") ;
+	scanf("%i",&x[1]) ;
+	printf("Masukkan NIM 3 : ") ;
+	scanf("%i",&x[2]) ;
+	printf("Masukkan NIM 4 : ") ;
+	scanf("%i",&x[3]) ;
+printf(“Masukkan NIM 5 : “);
+scanf(“%i”,&x[4]);
+printf(“Masukkan NIM 6 :");
+scanf(“%i”,&x[5]);
+	printf("Masukkan NIM 7: ") ;
+	scanf("%i",&x[6]) ;
+		printf("Masukkan NIM 8 : ") ;
+	scanf("%i",&x[7]) ;
+		printf("Masukkan NIM 9 : ") ;
+	scanf("%i",&x[8]) ;
+		printf("Masukkan NIM 10 : ") ;
+	scanf("%i",&x[9]) ;
+	
+	printf("\n") ;
+	printf("NIM ke-1 = %i\n",x[0]) ;
+	printf("NIM ke-2 = %i\n",x[1]) ;
+	printf("NIM ke-3 = %i\n",x[2]) ;
+	printf("NIM ke-4 = %i\n",x[3]) ;
+	printf(“NIM ke-5 = %i\n”,x[4];
+printf(“NIM ke-6 = %i\n”,x[5];
+printf("NIM ke-7 = %i\n",x[6]) ;
+printf("NIM ke-8 = %i\n",x[7]) ;
+printf("NIM ke-9= %i\n",x[8]) ;
+printf("NIM ke-10 = %i\n",x[9]) ;
+	return 0;
+}
+
