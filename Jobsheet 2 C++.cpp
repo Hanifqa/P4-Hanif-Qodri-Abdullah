467c57b28820e21eacb8c38f4c4e994fe9b65b75
Hanifqa <64571819+Hanifqa@users.noreply.github.com>
2020-05-11 22:39:43 +0800

    Create Jobsheet 2 C++.cpp
    Mengisi dan menampilkan array dinamis

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    string x [10];
+     cout<<"nim mahasiswa 1 : ";
+	 cin>>x[0];
+	 cout<<"nim mahasiswa 2 : ";
+	 cin>>x[1];
+	 cout<<"nim mahasiswa 3 : ";
+	 cin>>x[2];
+	 cout<<"nim mahasiswa 4 : ";
+	 cin>>x[3];
+	cout<<"nim mahasiswa 5 : ";
+
+	cin>>x[4];
+	cout<<"nim mahasiswa 6 : ";
+cin>>x[5];
+   cout<<"nim mahasiswa 7 :";
+   cin>>x[6];
+   cout<<"nim mahasiswa 8:";
+   cin>>x[7];
+   cout<<"nim mahasiswa 9:";
+   cin>>x[8];
+   cout<<"nim mahasiswa 10";
+   cin>>x[9];
+	 cout<<endl;
+	 cout<<"nim ke_1 = "<<x[0]<<endl;
+	 cout<<"nim ke_2 = "<<x[1]<<endl;
+	 cout<<"nim ke_3 = "<<x[2]<<endl;
+	 cout<<"nim ke_4 = "<<x[3]<<endl;
+	 cout<<"nim ke_5 = "<<x[4]<<endl;
+	cout<<"nim ke_6 = "<<x[5]<<endl;
+		cout<<"nim ke_7 = "<<x[6]<<endl;
+			cout<<"nim ke_8 = "<<x[7]<<endl;
+				cout<<"nim ke_9 = "<<x[8]<<endl;
+					cout<<"nim ke_10= "<<x[9]<<endl;
+	 return 0;
+}
